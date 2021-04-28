d4775ba37d03aa9549b522ef79fe05287d2d6012
Charles <charleslanop@gmail.com>
2021-04-28 13:20:35 -0300

    updated

--- assets/js/sidenav.js
+++ assets/js/sidenav.js
@@ -1,5 +1,25 @@
 const sidenav = () => {
+    sidenavDetails();
+    sidenavMenu();
+    sidenavChat();
+}
+
+const sidenavDetails = () => {
     const elements = document.querySelector('.sidenav-details');
+    M.Sidenav.init(elements, {
+        edge: 'right'
+    });
+}
+
+const sidenavMenu = () => {
+    const elements = document.querySelector('.sidenav-menu');
+    M.Sidenav.init(elements, {
+        edge: 'left'
+    });
+}
+
+const sidenavChat = () => {
+    const elements = document.querySelector('.sidenav-chat');
     M.Sidenav.init(elements, {
         edge: 'right'
     });
